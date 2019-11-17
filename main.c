e7da02c0dd7d0839d3aa706614152de7156210e5
Fábián Gábor <info@fabiangabor.com>
2019-11-17 19:28:53 +0100

    Palindrom works with most special characters

--- main.c
+++ main.c
@@ -40,16 +40,33 @@ void print_array(int arr[], int n)
     printf("\n");
 }
 
-int palindrome (char *p_string)
+int palindrome (char string[])
 {
+    char *p_string = string;
     int palindrom = 1;
     unsigned long long i = 0, j = 0, str_len = strlen(p_string);
+    char punctuation[] = " ,.'";
+    unsigned long p = 0;
+    char left;
+    char right;
 
-    while ( (i <= str_len/2) && palindrom )
+
+
+    while ( ((i <= str_len/2) || (j<= str_len/2)) && palindrom )
     {
-        if ( *(p_string + i) == ' ') i++;
-        if ( *(p_string + str_len - 1 - j) == ' ') j++;
-        if ( *(p_string + i) == *(p_string + str_len - 1 - j) )
+        left = *(p_string + i);
+        right= *(p_string + str_len - 1 - j);
+
+        while (left - 'A' < 0) {
+            i++;
+            left = *(p_string + i);
+        }
+        while (right - 'A' < 0) {
+            j++;
+            right= *(p_string + str_len - 1 - j);
+        }
+
+        if ( tolower(*(p_string + i)) == tolower(*(p_string + str_len - 1 - j)) )
         {
             i++;
             j++;
@@ -83,10 +100,10 @@ int main()
         printf("%d ", *(p_arr+i-1));
 
     //4. Készíts függvényt, ami a paraméterül kapott karaktertömbben lévő szövegről mutató segítsével eldönti és eredményül visszaadja, hogy az megegyezik-e a visszafelé olvasott változatával! (Palindrom)
-    char string[255] = "12 3 432 1";
+    char string[255] = "Go hang a salami I'm a lasagna hog";
     char *p_string;
     p_string = string;
-    printf("Palindrom: %s \n", (palindrome(p_string))?"IGEN":"NEM");
+    printf("Palindrom: %s \n", (palindrome(string))?"IGEN":"NEM");
 
     return 0;
 }
